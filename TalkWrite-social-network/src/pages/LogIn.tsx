c1fdc72350e7e78001fbbc6ab8d1bade7ed1b56b
Sasa Nedic <svetasmak0@gmail.com>
2024-06-12 20:36:49 +0200

    Create LogIn component

---
+++ TalkWrite-social-network/src/pages/LogIn.tsx
@@ -0,0 +1,22 @@
+
+
+const LogIn = () => {
+  return (
+    <div>
+      <h1>TalkWrite</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default LogIn;
