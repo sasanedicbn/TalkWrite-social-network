e68089580b3676c2e39c0326d96fcaceefd43e4a
Sasa Nedic <svetasmak0@gmail.com>
2024-06-13 17:16:56 +0200

    Create basic react hook form

--- TalkWrite-social-network/src/pages/LogIn.tsx
+++ TalkWrite-social-network/src/pages/LogIn.tsx
@@ -1,17 +1,27 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 
+type FormFields = {
+    email: string,
+    password: string,
+}
 
 const LogIn = () => {
+  const {register, handleSubmit} = useForm<FormFields>()
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data)
+  }
   return (
-    <div className="logIn-container">
+    <div className="logIn-container" onSubmit={handleSubmit(onSubmit)}>
       <h1>TalkWrite</h1>
       <form className="logIn-form">
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input {...register('email')} type="email" id="email" name="email" required />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input {...register('password')} type="password" id="password" name="password" required />
         </div>
         <button type="submit">Submit</button>
       </form>
