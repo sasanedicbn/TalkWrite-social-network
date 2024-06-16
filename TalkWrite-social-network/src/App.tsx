7003260cc01dede808b6f812f6ef9049c05c58a5
Sasa Nedic <svetasmak0@gmail.com>
2024-06-17 01:12:33 +0200

    Create ProtectedRoute component

--- TalkWrite-social-network/src/App.tsx
+++ TalkWrite-social-network/src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import LogIn from './pages/LogIn';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import store from './store/store';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const router = createBrowserRouter([
   {
@@ -13,10 +14,13 @@ const router = createBrowserRouter([
   },
   {
     path: '/home',
-    element: <Home/>,
+    element:
+    <ProtectedRoute>
+      <Home/>
+    </ProtectedRoute>,
   }
 ]);
-
+// protectedrouter zamotom home i ako ima token home me posalji ako nema login da idem samo ta logika {children }
 function App() {
   return (
     <Provider store={store}>
