95e684c729ee99946f98fa890ead1ecd54170f0b
Sasa Nedic <svetasmak0@gmail.com>
2024-06-17 14:01:17 +0200

    Create chagnes

--- TalkWrite-social-network/src/api/LogInApi.ts
+++ TalkWrite-social-network/src/api/LogInApi.ts
@@ -1,6 +1,6 @@
 
 
-export const handleLogin = async (data, navigate, setError) => {
+export const handleLogin = async (data, navigate) => {
     const baseUrl = 'https://api.hr.constel.co/api/v1';
 
     try {
@@ -13,30 +13,20 @@ export const handleLogin = async (data, navigate, setError) => {
         });
       
         if (!response.ok) {
-            const errorData = await response.json();
-            let errorMessage = 'Login failed';
+            const {status, error} = await response.json();
+            // let errorMessage = 'Login failed';
 
             if (response.status === 400) {
                 console.log(response.status)
-                errorMessage = errorData.message || 'Incorect password';
-            } else if (response.status === 401) {
-                errorMessage = 'Unauthorized';
-            } else if (errorData.message) {
-                errorMessage = errorData.message;
-            }
-
-            throw new Error(errorMessage);
+                console.log(status,error)
+              return {status,error}
         }
-
+    }
         const {token} = await response.json();
         navigate('/home');
         console.log(token)
         localStorage.setItem('jwt', token)
     } catch (error) {
         console.error('Login error:', error);
-        setError("root", {
-            type: "manual",
-            message: error.message || "Something went wrong",
-        });
     }
 };
--- TalkWrite-social-network/src/store/userSlice.ts
+++ TalkWrite-social-network/src/store/userSlice.ts
@@ -2,16 +2,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    getUser: (state) => {
-     
-    },
+  
   },
 });
 
