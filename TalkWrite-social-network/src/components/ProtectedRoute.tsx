dc9db555191d4509f89448cb62655df1d3bca663
Sasa Nedic <svetasmak0@gmail.com>
2024-06-18 21:33:24 +0200

    Create SinglePost

--- TalkWrite-social-network/src/components/ProtectedRoute.tsx
+++ TalkWrite-social-network/src/components/ProtectedRoute.tsx
@@ -1,8 +1,10 @@
 import { Navigate } from 'react-router-dom';
 import { UserApi } from '../api/UserApi';
 import { UserPosts } from '../api/UserPosts';
+import { useDispatch } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch()
   const token = localStorage.getItem('jwt');
   
   if (!token) {
@@ -10,6 +12,7 @@ const ProtectedRoute = ({ children }) => {
   } else{
     UserApi()
     UserPosts()
+    
   }
 //ako nema tokena i juzera userApi()
   return children;
