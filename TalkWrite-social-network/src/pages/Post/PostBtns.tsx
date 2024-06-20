37c2454bdd9edbddfc45434705fd1dc0c54c337d
Sasa Nedic <svetasmak0@gmail.com>
2024-06-20 20:43:16 +0200

    Conditional show classname

--- TalkWrite-social-network/src/pages/Post/PostBtns.tsx
+++ TalkWrite-social-network/src/pages/Post/PostBtns.tsx
@@ -1,12 +1,18 @@
-import { FaRegComment, FaRegHeart } from "react-icons/fa";
+import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 
-const PostBtns = ({liked,likes,comments}) => {
-    console.log(liked,likes,comments)
-    return(
+const PostBtns = ({ liked, likes, comments }) => {
+    console.log(liked, likes, comments);
+
+    return (
         <div id="postsBtns-container">
-            <button className="btn-like"> <FaRegHeart size={24} className='like-icon' />{likes}</button>
-            <button className="btn-comment"> <FaRegComment size={24} className='comment-icon' />{comments}</button>
+            <button className={liked ? 'btn-liked' : 'btn-like'}>
+                <FaRegHeart size={24} className='like-icon' />{likes}
+            </button>
+            <button className="btn-comment">
+                <FaRegComment size={24} className='comment-icon' />{comments}
+            </button>
         </div>
-    )
+    );
 }
+
 export default PostBtns;
