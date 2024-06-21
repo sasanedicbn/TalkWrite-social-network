1bd896014694aee21e1d61152e7ad22de7e34aff
Sasa Nedic <svetasmak0@gmail.com>
2024-06-21 19:31:29 +0200

    Create changes

--- TalkWrite-social-network/src/pages/Post/PostBtns.tsx
+++ TalkWrite-social-network/src/pages/Post/PostBtns.tsx
@@ -1,27 +1,32 @@
 import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
-import { toggleLike } from '../../store/postsSlice';
 import { useState } from 'react';
+import ClickedPost from './ClickedPost';
 
 const PostBtns = ({ liked, likes, comments, post_id }) => {
-    const [showClickedPost, setshowClickedPost] = useState(false)
+    const [activeId, setActiveId] = useState(null);
     const dispatch = useDispatch();
-    const toggleLikes = () => {
-        dispatch(toggleLike(post_id))
-    }
-    console.log(liked, likes, comments, post_id);
-    const showPostHandler = () => {
-        setshowClickedPost(state => !state)
-    }
+    
+    // const toggleLikes = () => {
+    //     dispatch(toggleLike(post_id))
+    // }
+    // console.log(liked, likes, comments, post_id);
+
+    const showPostHandler = (id) => {
+        setActiveId(id);
+    };
+
+    console.log(post_id, activeId);
 
     return (
         <div id="postsBtns-container">
-            <button className={liked ? 'btn-liked' : 'btn-like'} onClick={showPostHandler}>
+            <button className={liked ? 'btn-liked' : 'btn-like'} onClick={() => showPostHandler(post_id)}>
                 <FaRegHeart size={24} className='like-icon' />{likes}
             </button>
             <button className="btn-comment">
                 <FaRegComment size={24} className='comment-icon' />{comments}
             </button>
+            {post_id === activeId && <ClickedPost post_id={post_id} />}
         </div>
     );
 }
