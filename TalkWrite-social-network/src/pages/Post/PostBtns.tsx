f845a662754d150462d29e5c0722f205a1c67d00
Sasa Nedic <svetasmak0@gmail.com>
2024-06-21 15:42:42 +0200

    Create more code

--- TalkWrite-social-network/src/pages/Post/PostBtns.tsx
+++ TalkWrite-social-network/src/pages/Post/PostBtns.tsx
@@ -1,17 +1,22 @@
 import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { toggleLike } from '../../store/postsSlice';
+import { useState } from 'react';
 
 const PostBtns = ({ liked, likes, comments, post_id }) => {
+    const [showClickedPost, setshowClickedPost] = useState(false)
     const dispatch = useDispatch();
     const toggleLikes = () => {
         dispatch(toggleLike(post_id))
     }
     console.log(liked, likes, comments, post_id);
+    const showPostHandler = () => {
+        setshowClickedPost(state => !state)
+    }
 
     return (
         <div id="postsBtns-container">
-            <button className={liked ? 'btn-liked' : 'btn-like'} onClick={toggleLikes}>
+            <button className={liked ? 'btn-liked' : 'btn-like'} onClick={showPostHandler}>
                 <FaRegHeart size={24} className='like-icon' />{likes}
             </button>
             <button className="btn-comment">
