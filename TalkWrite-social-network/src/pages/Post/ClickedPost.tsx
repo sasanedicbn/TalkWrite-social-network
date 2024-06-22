7bd3a2474c20d31ceebc7fd67f8429ead85e0df9
Sasa Nedic <svetasmak0@gmail.com>
2024-06-23 01:18:34 +0200

    Create changes

--- TalkWrite-social-network/src/pages/Post/ClickedPost.tsx
+++ TalkWrite-social-network/src/pages/Post/ClickedPost.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import HeaderPosts from "./HeaderPosts";
 import ContentPost from "./ContentPost";
 import Comment from "./Comment";
+import PostBtns from "./Btns/PostBtns";
 
 const ClickedPost = () => {
     const post = useSelector(state => state.posts.singlePost.post);
@@ -23,6 +24,7 @@ const ClickedPost = () => {
                   <HeaderPosts post={post} />
                   <ContentPost image={post.image} text={post.text} />
                   <Comment/>
+                  <PostBtns liked={post.liked} likes={post.likes} comments={post.comment} post_id={post.post_id} />
                 </div>
             </div>
         </div>
