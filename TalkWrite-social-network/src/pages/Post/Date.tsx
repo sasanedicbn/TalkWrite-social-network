4820ac963472ffedeff5a893e23fdfbe48a35454
Sasa Nedic <svetasmak0@gmail.com>
2024-06-27 19:32:23 +0200

    Create changes in Date component

--- TalkWrite-social-network/src/pages/Post/Date.tsx
+++ TalkWrite-social-network/src/pages/Post/Date.tsx
@@ -2,26 +2,24 @@ import { FaRegCalendarAlt, FaTrash } from "react-icons/fa";
 import { formatedDate } from "../../constants/constants";
 import { useSelector, useDispatch } from "react-redux";
 import deletePostApi from "../../api/DeletePostApi";
-import { deletePost, getPosts } from "../../store/postsSlice";
+import deleteCommentApi from "../../api/DeleteCommentApi";
+import { deletePost, getPosts, deleteComment, getComments } from "../../store/postsSlice";
 import { UserPosts } from "../../api/UserPosts";
-
+import { PostComments } from "../../api/PostComments"; // Pretpostavimo da imate API za dobijanje komentara
 
 const Date = ({ post }) => {
   const { user } = post;
   const userName = useSelector(state => state.user.setUser.username);
-  const comments = useSelector(state => state.posts.comments)
-  const [commentsUserName] = comments.map((comment) => comment.username )
-  console.log('zabrisanjeusernam',commentsUserName)
+  const comments = useSelector(state => state.posts.comments);
   const dispatch = useDispatch();
-  
 
   const handleDeletePost = async () => {
     try {
       const status = await deletePostApi(post.post_id);
-      const posts = await UserPosts()
+      const posts = await UserPosts();
       if (status === 200) {
         dispatch(deletePost(post.post_id));
-        dispatch(getPosts(posts))
+        dispatch(getPosts(posts));
       } else {
         console.error('Failed to delete post:', status);
       }
@@ -29,18 +27,36 @@ const Date = ({ post }) => {
       console.error('Error deleting post:', error);
     }
   };
-  const handleDeleteComment = async () => {
-    console.log('comment brise ovo')
-  }
+
+  const handleDeleteComment = async (commentId) => {
+    try {
+      const status = await deleteCommentApi(commentId);
+      const comments = await PostComments(post.post_id); // Pretpostavimo da imate API za dobijanje komentara
+      if (status === 200) {
+        dispatch(deleteComment(commentId));
+        dispatch(getComments(comments));
+      } else {
+        console.error('Failed to delete comment:', status);
+      }
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+    }
+  };
 
   return (
     <div className="date-container">
       <p className='userPost-date'><FaRegCalendarAlt size={24} />{formatedDate(post.created_at)}</p>
-
-      {user.username  === userName ? <p id="date-delete" onClick={handleDeletePost}><FaTrash /> Delete</p> : 
-       commentsUserName === userName ? <p id="date-delete" onClick={handleDeleteComment}><FaTrash /> Delete</p> : ''
-      }
-  
+      {user.username === userName ? (
+        <p id="date-delete" onClick={handleDeletePost}><FaTrash /> Delete</p>
+      ) : (
+        comments.map(comment => (
+          comment.username === userName && (
+            <p key={comment.comment_id} id="date-delete" onClick={() => handleDeleteComment(comment.comment_id)}>
+              <FaTrash /> Delete
+            </p>
+          )
+        ))
+      )}
     </div>
   );
 };
