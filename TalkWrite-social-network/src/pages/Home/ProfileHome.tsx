4e784c57352399d81a05ff6fb5d56685a7bb41ed
Sasa Nedic <svetasmak0@gmail.com>
2024-07-05 00:17:31 +0200

    Create toggleDownMenu function

--- TalkWrite-social-network/src/pages/Home/ProfileHome.tsx
+++ TalkWrite-social-network/src/pages/Home/ProfileHome.tsx
@@ -4,11 +4,13 @@ import DropDownMenu from "../../components/DropDownMenu";
 const ProfileHome = () => {
     const [showDownMenu, setShowDownMenu] = useState(false)
 
-  
+    const toggleDownMenu = () => {
+       setShowDownMenu(state => !state)
+    }
     return(
         <>
          <div className="container-profileHome">
-              <img src="./public/profile.img" className="profile-img"/>
+              <img src="./public/profile.img" className="profile-img" onClick={toggleDownMenu}/>
          </div>
          {showDownMenu && <DropDownMenu/>}
         </>
