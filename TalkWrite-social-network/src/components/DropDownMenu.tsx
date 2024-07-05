76afe2911ba06277b86b7c7f8889334fc275ae97
Sasa Nedic <svetasmak0@gmail.com>
2024-07-05 02:06:08 +0200

    Create responsive for 1000px

--- TalkWrite-social-network/src/components/DropDownMenu.tsx
+++ TalkWrite-social-network/src/components/DropDownMenu.tsx
@@ -1,24 +1,26 @@
 import { FaSignOutAlt, FaUser } from "react-icons/fa";
-import { useHistory } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 const DropDownMenu = () => {
+    const navigate = useNavigate();
+
     const logOutUser = () => {
-        const history = useHistory()
-        localStorage.removeItem('jwt')
-        history.push('/')
-        
-    }
-    return(
+        localStorage.removeItem('jwt');
+        navigate('/');
+    };
+
+    return (
         <div className="dropDownMenu">
-          <div>
-            <FaUser/>
-            <p>My Profile</p>
-          </div>
-          <div>
-            <FaSignOutAlt/>
-            <p>Log out</p>
-          </div>
+            <div>
+                <FaUser />
+                <p>My Profile</p>
+            </div>
+            <div onClick={logOutUser}>
+                <FaSignOutAlt />
+                <p>Log out</p>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default DropDownMenu;
