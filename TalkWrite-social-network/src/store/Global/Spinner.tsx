8c95e11008b8afca4d9b76a060b5786cd4537252
Sasa Nedic <svetasmak0@gmail.com>
2024-07-05 21:25:56 +0200

    Create spinner component

---
+++ TalkWrite-social-network/src/store/Global/Spinner.tsx
@@ -0,0 +1,11 @@
+import { FaSpinner } from 'react-icons/fa';
+
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <FaSpinner className="spinner-icon" />
+    </div>
+  );
+};
+
+export default Spinner;
